30898768e4f3c43bd256f4e5a499924e043f5efb
Thanosdi <thanosdi@live.com>
2016-10-13 19:13:36 +0300

    Show only if photo exist

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,8 +9,8 @@
 		<input type="text" name="search"  class="form-control" placeholder="Search goddamit" ng-model="search" ng-model-options="{ debounce: 300 }"/>
 
 		<div class="col-md-12 cards">
-			<div class="col-md-4 card" ng-repeat="card in cards">
-				<h2>@{{card.name}} (@{{card.power}})</h2>
+			<div class="col-md-4 card" ng-repeat="card in cards" ng-if="card.imageUrl">			
+				<h2>@{{card.name}} <span ng-if="card.power">[@{{card.power}}]</span></h2>
 				<img src="@{{card.imageUrl}}"/>
 			</div>
 		</div>
